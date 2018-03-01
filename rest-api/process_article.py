9e34207f0e008405c3cf7799b50e4471fced202f
alex-morikado <asmorikado+git@gmail.com>
2018-03-01 10:33:58 -0600

    correct indices on process article and added support for quotes

--- rest-api/process_article.py
+++ rest-api/process_article.py
@@ -35,46 +35,64 @@ def get_all_claims(article):
         if 'NNP' in tagged[i][1]:
             claim_text = tagged[i][0]
             current_length = total_length
+            current_length += len(tagged[i][0]) + 1
             i += 1
             #treat adjacent proper nouns as one claim
             while 'NNP' in tagged[i][1] or 'POS' == tagged[i][1]:
                 if 'POS' == tagged[i][1]:
-                    total_length += len(tagged[i][0])
+                    current_length += len(tagged[i][0])
                 else:
-                    total_length += len(tagged[i][0]) + 1
+                    current_length += len(tagged[i][0]) + 1
                     claim_text += ' '
                 claim_text += tagged[i][0]
                 i += 1
             new_claim = {'article_id': article['id'],
                         'text': claim_text,
                         'claim_type': NOUN,
-                        'start_index': current_length,
+                        'start_index': total_length,
                         }
             claims.append(new_claim)
-            current_length = total_length
+            total_length = current_length
         #number
         elif 'CD' == tagged[i][1]:
             new_claim = {'article_id': article['id'],
                         'text': tagged[i][0],
                         'claim_type': NUMBER,
-                        'start_index': total_length
+                        'start_index': total_length,
                         }
             claims.append(new_claim)
             total_length += len(tagged[i][0]) + 1
             i += 1
-
-        else:
-            total_length += len(tagged[i][0]) + 1
-            i += 1
         #quote
-        """
         elif '``' == tagged[i][1]:
-            text = '"'
+            claim_text = '"'
+            current_length = total_length
+            current_length += 1
+            i += 1
             while tagged[i][1] != "''" and i < num_tags:
                 current_length += len(tagged[i][0])
+                claim_text += tagged[i][0]
                 if tagged[i][0].isalnum():
                     current_length += 1
-        """
+                    claim_text += ' '
+                i += 1
+            claim_text = claim_text[:-1] + '"'
+            current_length += 1
+            i += 1
+
+            new_claim = {'article_id': article['id'],
+                        'text': claim_text,
+                        'claim_type': QUOTE,
+                        'start_index': total_length,
+                        }
+            claims.append(new_claim)
+            total_length = current_length
+
+        else:
+            total_length += len(tagged[i][0])
+            if tagged[i][0].isalnum():
+                total_length += 1
+            i += 1
 
 
     return claims
@@ -97,7 +115,9 @@ def filter_claims(claims, type_id):
 
 if __name__ == '__main__':
     test = 'The White House is "actively considering issuing a veto threat" against the 51 to 49 House vote.'
-    print('\n\n')
-    print(pos_tag(word_tokenize(test)))
     print('\n')
-    print(get_all_claims({'text': test, 'id': 1}))
+    claims = get_all_claims({'text': test, 'id': 1})
+    print(claims)
+    print('\n\n')
+    for claim in claims:
+        print(test[claim['start_index']:claim['start_index']+ len(claim['text'])])
